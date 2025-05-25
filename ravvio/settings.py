bb8b41aa6c5cf37c2b1e7da448395c2b36e89bc5
rebin <muhammedrebin1794@gmail.com>
2025-05-25 17:39:36 +0530

    refactor(settings): use environment variables for SECRET_KEY and DEBUG

--- ravvio/settings.py
+++ ravvio/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&c_j89(6+xnxhu1#9j*p6925n!nz#+1qe*btqfxelbeu83=cd('
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = []
 

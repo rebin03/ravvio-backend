45ae9f790c9e1562492ff0e15edc5e5485a75746
Muhammed Rebin <muhammedrebin1794@gmail.com>
2025-07-28 23:12:07 +0530

    update domain name

--- ravvio/settings.py
+++ ravvio/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['localhost', 'ravvio-backend.onrender.com', 'https://api.ravvio.net/']
+ALLOWED_HOSTS = ['localhost', 'ravvio-backend.onrender.com', 'api.ravvio.net']
 
 # Application definition
 
